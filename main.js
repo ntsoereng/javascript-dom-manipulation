b974f89d005b45a61b5028fb2c988541ab5c829f
ntsoereng <35761040+ntsoereng@users.noreply.github.com>
2022-10-26 13:12:44 +0200

    Basic drag and drop algorithm

---
+++ main.js
@@ -0,0 +1,34 @@
+const ball = document.querySelector('#ball');
+
+ball.onmousedown = function (event) {
+  // (1) PREPARE to move: make absolute and on top by z-index
+  ball.style.position = 'absolute';
+  ball.style.zIndex = 1000;
+
+  /* Move it out of any current parents directly into body
+  to make it positioned relative to the body
+  */
+  document.body.append(ball);
+
+  // Center the ball at (pageX, pageY) coordinates
+  function moveAt(pageX, pageY) {
+    ball.style.left = pageX - ball.offsetWidth / 2 + 'px';
+    ball.style.top = pageY - ball.offsetHeight / 2 + 'px';
+  }
+
+  // move our absolutely positioned ball UNDER THE POINTER
+  moveAt(event.pageX, event.pageY);
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  // (2) MOVE the ball on mousemove
+  document.addEventListener('mousemove', onMouseMove);
+
+  // (3) DROP the ball, remove unnecessary handlers
+  ball.onmouseup = function () {
+    document.removeEventListener('mousemove', onMouseMove);
+    ball.onmouseup = null;
+  };
+};
