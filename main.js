ad67c896e6ffd304e3874fa437a79046670f8744
ntsoereng <35761040+ntsoereng@users.noreply.github.com>
2022-10-26 13:48:45 +0200

    Update algorithm for proper positioning

--- main.js
+++ main.js
@@ -6,18 +6,19 @@ ball.ondragstart = function () {
 
 ball.onmousedown = function (event) {
   // (1) PREPARE to move: make absolute and on top by z-index
+  const shiftX = event.clientX - ball.getBoundingClientRect().left;
+  const shiftY = event.clientY - ball.getBoundingClientRect().top;
+
   ball.style.position = 'absolute';
   ball.style.zIndex = 1000;
-
-  /* Move it out of any current parents directly into body
-  to make it positioned relative to the body
-  */
+  // Move it out of any current parents directly into body
+  // to make it positioned relative to the body
   document.body.append(ball);
 
   // Center the ball at (pageX, pageY) coordinates
   function moveAt(pageX, pageY) {
-    ball.style.left = pageX - ball.offsetWidth / 2 + 'px';
-    ball.style.top = pageY - ball.offsetHeight / 2 + 'px';
+    ball.style.left = pageX - shiftX + 'px';
+    ball.style.top = `${pageY - shiftY}px`;
   }
 
   // move our absolutely positioned ball UNDER THE POINTER
